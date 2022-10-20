16bd4a22066b113a7c5bec81e83ecd60dbe44f1c
Boston343 <branden.austin1@gmail.com>
2022-10-19 20:28:29 -0400

    initial files and setup

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # BlogWebsite
-Blog Website - EJS challenge
+
+Blog Website - used as an EJS challenge project. This uses express.js and EJS.
+
+-------- below has not been updated --------
+
+You can add items to the list, and cross them out. Paths available include `localhost:3000/`, `localhost:3000/work`, and `localhost:3000/about`. You can clone this repo and install the node modules below to test on your local computer. 
+
+## Dependencies
+
+-   Node modules - inside project run `npm install express ejs`
+    -   express
+    -	ejs
+    -   eslint - if desired for linting 
+    	-   [ESLint Getting Started Guide](https://eslint.org/docs/latest/user-guide/getting-started)
+
+## Includes
+
+-   JS includes
+    -   express
+    -   path
+    -   url
+-   EJS - Data retreival and manipulation
+    -   Serving up HTML files with input from server
+    -   Retreive data from form, manipulate, and respond to user with updated html file
+    -   EJS layouts and running code inside `.ejs` files so we don't have to have a ton of html files
---
+++ app.js
@@ -0,0 +1,47 @@
+// npm and express includes
+import express from "express"; // npm install express
+import path from "path";
+import { fileURLToPath } from "url";
+// import https from "https"; // for forming external get requests
+
+// local includes
+// import * as date from "./src/date.js";
+
+const app = express();
+app.set("view engine", "ejs"); // using EJS
+const port = process.env.PORT || 3000;
+app.use(express.urlencoded({ extended: true })); // this is for parsing data from html form
+
+// __dirname is only available with CJS. Since I am using ESM I need to get it another way
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// static items like other js or css files will not load unless you define where the server should
+//      start looking for those files.
+app.use(express.static(path.join(__dirname, "/public")));
+
+// text constants for webpages
+const homeStartingContent =
+    "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const aboutContent =
+    "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const contactContent =
+    "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
+// -----------------------------------------------------------------------------------
+// ---------------------------------- Listening --------------------------------------
+// -----------------------------------------------------------------------------------
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
+
+// -----------------------------------------------------------------------------------
+// --------------------------------- Get Requests ------------------------------------
+// -----------------------------------------------------------------------------------
+app.get("/", (req, res) => {
+    // console.log("Server is up and running.");
+});
+
+// -----------------------------------------------------------------------------------
+// -------------------------------- Post Requests ------------------------------------
+// -----------------------------------------------------------------------------------
